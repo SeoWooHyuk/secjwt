2f46a4b66ba698f6bdfbab56725289cc86402422
서우혁 <tjdngur123@naver.com>
2023-06-22 11:59:33 +0900

    스프링 시큐리티 +jwt + 타임리프트작업

---
+++ src/test/java/com/spring/secjwt/SecjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.secjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
